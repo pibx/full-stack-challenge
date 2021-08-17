c7433ba4e8dc9aec8974bb63d7d5a886749174ec
Nick <nick.scharold@guinnpartners.com>
2021-08-17 16:28:10 -0600

    remove build output from git

--- common/.build/util/decode.d.ts
+++
@@ -1,5 +0,0 @@
-import { Decoder } from "io-ts";
-export declare const decode: <T>(Codec: Decoder<any, T>, value: unknown) => T;
-export declare class DecodeError extends Error {
-    name: string;
-}
